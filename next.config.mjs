a15fe73e828ee100ecf2ffba0b3798c7d7506617
Niba farooq <164494729+Niba-khan@users.noreply.github.com>
2025-02-03 19:01:35 +0500

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['i.pinimg.com',"cdn-icons-png.freepik.com" ,"img.lovepik.com",'st4.depositphotos.com','t4.ftcdn.net','cdn-icons-png.flaticon.com','e7.pngegg.com','img.freepik.com','t4.ftcdn.net','img.freepik.com','encrypted-tbn0.gstatic.com',"cdn.create.vista.com"]
+        domains:["w7.pngwing.com",'i.pinimg.com',"cdn-icons-png.freepik.com" ,"img.lovepik.com",'st4.depositphotos.com','t4.ftcdn.net','cdn-icons-png.flaticon.com','e7.pngegg.com','img.freepik.com','t4.ftcdn.net','img.freepik.com','encrypted-tbn0.gstatic.com',"cdn.create.vista.com"]
     }
 };
 
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,5 @@
 import styles from "./contact.module.css";
-import { FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
+import Image from "next/image";
 
 const Contact = () => {
   return (
@@ -16,15 +16,15 @@ const Contact = () => {
 
           <div>
             <div className={styles.iconRow}>
-              <FaEnvelope color="blue" size={20}  />
+              <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPK9ylMDzbfPj6Ldsjijgn8lymX7mOqX9h2Q&s" alt="Email" width={20} height={20} className={styles.icon} />
               <p>Contact@GreatStack.dev</p>
             </div>
             <div className={styles.iconRow}>
-              <FaPhoneAlt color="blue" size={20} />
+              <Image src="https://e7.pngegg.com/pngimages/547/530/png-clipart-telephone-call-email-computer-icons-email-miscellaneous-text-thumbnail.png" alt="Phone" width={20} height={20} className={styles.icon} />
               <p>+1 123-456-7890</p>
             </div>
             <div className={styles.iconRow}>
-              <FaMapMarkerAlt color="blue" size={20} />
+              <Image src="https://w7.pngwing.com/pngs/158/65/png-transparent-location-application-icon-computer-software-business-information-organization-location-logo-miscellaneous-blue-company-thumbnail.png" alt="Location" width={20} height={20} className={styles.icon} />
               <p>77 Massachusetts Ave, Cambridge, MA 02139, USA</p>
             </div>
           </div>
