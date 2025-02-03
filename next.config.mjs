0889156fb6968ea96ac31e1e1a2aa4cedc27818f
Niba farooq <164494729+Niba-khan@users.noreply.github.com>
2025-02-03 18:38:42 +0500

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        domains:['i.pinimg.com',"cdn-icons-png.freepik.com" ,"img.lovepik.com",'st4.depositphotos.com','t4.ftcdn.net','cdn-icons-png.flaticon.com','e7.pngegg.com','img.freepik.com','t4.ftcdn.net','img.freepik.com','encrypted-tbn0.gstatic.com',"cdn.create.vista.com"]
+    }
+};
 
 export default nextConfig;
